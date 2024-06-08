9104ea0d5111d021a009a108e601f45794a38565
okikura108 <okikura.a.azusa.o@gmail.com>
2024-06-08 13:58:21 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,13 @@
+public abstract clss TangibleAsset{
+  private String name;
+  private int price;
+  private String color;
+
+public TangibleAsset(String name,int price String color){
+  this.name =name;
+  this.price =price;
+  this.color=color;
+}
+public String getName(){returm this.name;}
+public int getPrice(){returm this.price;}
+public String getColor(){returm this.color;}
